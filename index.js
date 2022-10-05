e527ec6ba50dc3d93418f2e35d56f0df3d1babdf
MohammedAbuEssa <mabuissa5@gmail.com>
2022-10-05 20:41:33 +0300

    First Upload on github

---
+++ index.js
@@ -0,0 +1,59 @@
+const buttons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < buttons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var button = this.innerHTML;
+  KeyPress(button);
+  ButtonCliked(button);
+  })
+}
+function KeyPress(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var snare = new Audio("sounds/crash.mp3");
+      snare.play();
+      break;
+    case "l":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    default:
+
+  }
+}
+
+
+
+document.addEventListener("keydown",function(event){
+  KeyPress(event.key);
+  ButtonCliked(event.key);
+} );
+
+
+
+function ButtonCliked(currentKey){
+var pressedKey=document.querySelector("."+currentKey);
+pressedKey.classList.add("pressed");
+setTimeout(function(){
+pressedKey.classList.remove("pressed");
+},100);
+}
